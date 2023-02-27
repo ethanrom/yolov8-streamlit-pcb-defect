1ae8b5878406d90abf002b29812cdb6fa3f70a2d
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-27 13:46:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import cv2
 import requests
 import os
 import numpy as np
+from tempfile import NamedTemporaryFile
 
 from ultralytics import YOLO
 
@@ -31,12 +32,24 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    output_image = show_preds_image(uploaded_file)
-    st.image(output_image, caption='Output Image.', use_column_width=True)
+    # Save the uploaded file to a temporary file
+    with NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.read())
+        tmp_file.flush()
 
-    if st.button("Save Image"):
-        cv2.imwrite("output_image.jpg", output_image)
-        st.success("Image saved successfully!")
+        # Load the image from the temporary file and show it
+        image = cv2.imread(tmp_file.name)
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
+
+        # Process the image and show the output
+        output_image = show_preds_image(tmp_file.name)
+        st.image(output_image, caption='Output Image.', use_column_width=True)
+
+        # Save the output image to disk
+        if st.button("Save Image"):
+            cv2.imwrite("output_image.jpg", output_image)
+            st.success("Image saved successfully!")
+            
+            # Remove the temporary file
+            os.unlink(tmp_file.name)
