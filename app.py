8e54818db2f433d80348eb9ae49614a3d6d6521d
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-27 13:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import cv2
 import requests
 import os
 import numpy as np
-from tempfile import NamedTemporaryFile
+import tempfile
 
 from ultralytics import YOLO
 
 model = YOLO('yolov8_pcb.pt')
-path = [['spur.jpg'], ['mouse.jpg']]
+#path = [['spur.jpg'], ['mouse.jpg']]
 
 def show_preds_image(image_path):
     image = cv2.imread(image_path)
@@ -32,24 +32,22 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Save the uploaded file to a temporary file
-    with NamedTemporaryFile(delete=False) as tmp_file:
+    # Save the uploaded file to a temporary location on the server
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         tmp_file.write(uploaded_file.read())
         tmp_file.flush()
 
-        # Load the image from the temporary file and show it
         image = cv2.imread(tmp_file.name)
         st.image(image, caption='Uploaded Image.', use_column_width=True)
         st.write("")
-
-        # Process the image and show the output
+        
         output_image = show_preds_image(tmp_file.name)
         st.image(output_image, caption='Output Image.', use_column_width=True)
 
-        # Save the output image to disk
         if st.button("Save Image"):
             cv2.imwrite("output_image.jpg", output_image)
             st.success("Image saved successfully!")
-            
-            # Remove the temporary file
-            os.unlink(tmp_file.name)
+            os.remove("output_image.jpg")  # remove the saved image from the server to avoid filling up disk space
+
+        # remove the temporary file after processing
+        os.remove(tmp_file.name)
